0a91dc409d61f2ca57cbf6acf281fc98840b0038
celishi <celiashi666@hotmail.com>
2025-01-11 12:48:51 -0500

    web without raycast, switched to cylinder object

--- Assets/Scripts/web.cs
+++ Assets/Scripts/web.cs
@@ -18,18 +18,26 @@ public class web : MonoBehaviour
     void SpawnWeb()
     {
         Vector3 pos = player.transform.position;
+
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = Camera.main.WorldToScreenPoint(pos).z;
         Vector3 worldMousPos = Camera.main.ScreenToWorldPoint(mousePos);
 
         Vector3 dir = worldMousPos - pos;
+/*        RaycastHit hit;
+
+        if (Physics.Raycast(pos, dir.normalized, out hit, dir.magnitude))
+        {
+            worldMousPos = hit.point;
+        }
+
+        dir = worldMousPos - pos;*/
         float distance = dir.magnitude;
-        Debug.Log("distance is: " + distance);
 
         GameObject web = Instantiate(webPrefab);
         web.transform.position = pos + dir / 2;
         web.transform.LookAt(worldMousPos);
-        web.transform.localScale = new Vector3(web.transform.localScale.x, web.transform.localScale.y, distance);
-        Debug.Log("scale is is: " + web.transform.localScale.x + ", " + web.transform.localScale.y + ", " + distance);
-    }
+        web.transform.Rotate(90, 0, 0);
+
+        web.transform.localScale = new Vector3(web.transform.localScale.x, distance/2, web.transform.localScale.z);    }
 }
