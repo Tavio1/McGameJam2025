445e8db2fb6253c7473ad836d5e7799a0130dc31
luna <68436560+lunarflare7@users.noreply.github.com>
2025-01-11 13:17:52 -0500

    added web interface

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,39 +22,61 @@ public class PlayerController : MonoBehaviour
     public float rayLength;
     public LayerMask groundLayerMask;
 
+    [Header("Web")]
+    private InputAction attachToWebAction;
+    public bool attached;
+    public float radiusToAttach;
+    public LayerMask webLayerMask;
+    public 
+
 
     void Start()
     {
         playerActions.Enable();
         moveAction = playerActions.FindActionMap("Player").FindAction("Move");
+        attachToWebAction = playerActions.FindActionMap("Player").FindAction("AttachToWeb");
+        attachToWebAction.performed += Attach;
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Attach(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("attach");
+        Collider[] cols = Physics.OverlapSphere(transform.position, radiusToAttach, webLayerMask);
+        if (cols[0] != null)
+        {
+        }
     }
 
     void FixedUpdate()
     {
-        moveDir = moveAction.ReadValue<float>();
-
-        RaycastHit hit;
-        Physics.Raycast(transform.position, Vector3.down, out hit, rayLength, groundLayerMask);
-
-        //if you're in the air
-        if (hit.normal == Vector3.zero)
+        if (!attached)
         {
-            rb.velocity = Vector3.MoveTowards(rb.velocity, new Vector3(moveDir * speed, rb.velocity.y, 0), Time.deltaTime * accel);
-        } 
-        //if you're on the ground
-        else
-        {
-            rb.velocity = Vector3.MoveTowards(rb.velocity, Vector3.Cross(hit.normal, Vector3.forward * moveDir).normalized * speed, Time.deltaTime * accel);
+            moveDir = moveAction.ReadValue<float>();
+
+            RaycastHit hit;
+            Physics.Raycast(transform.position, Vector3.down, out hit, rayLength, groundLayerMask);
+
+            //if you're in the air
+            if (hit.normal == Vector3.zero)
+            {
+                rb.velocity = Vector3.MoveTowards(rb.velocity, new Vector3(moveDir * speed, rb.velocity.y, 0), Time.deltaTime * accel);
+            }
+            //if you're on the ground
+            else
+            {
+                rb.velocity = Vector3.MoveTowards(rb.velocity, Vector3.Cross(hit.normal, Vector3.forward * moveDir).normalized * speed, Time.deltaTime * accel);
+            }
+
+
+            //slop snapping
+            if (prevOnSlope != SlopeCheck(hit))
+            {
+                rb.velocity = new Vector3(rb.velocity.x, -slopeSnapping, rb.velocity.z);
+            }
+
+            prevOnSlope = SlopeCheck(hit);
         }
-
-
-        //fix random slope nonsense
-        if(prevOnSlope != SlopeCheck(hit)) {
-            rb.velocity = new Vector3(rb.velocity.x, -slopeSnapping, rb.velocity.z);
-        }
-
-        prevOnSlope = SlopeCheck(hit);
     }
 
     bool SlopeCheck(RaycastHit hit)
