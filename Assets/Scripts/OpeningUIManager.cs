e00e4a92a9fba158631a8f75dadbf3077421850c
celishi <celiashi666@hotmail.com>
2025-01-12 07:59:21 -0500

    scene transition done (except in main)

--- Assets/Scripts/OpeningUIManager.cs
+++ Assets/Scripts/OpeningUIManager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.EditorCoroutines.Editor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class OpeningUIManager : MonoBehaviour
 {
     [SerializeField] GameObject Buttons;
-    public Animator transition;
+    public Animator start;
+    public Animator end;
+    public float duration;
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
+        end.SetTrigger("Start");
+
         LeanTween.alphaCanvas(Buttons.GetComponent<CanvasGroup>(), 1f, 1.5f)
         .setIgnoreTimeScale(true);
         LeanTween.moveY(Buttons,160f,1.5f)
@@ -27,9 +30,9 @@ public class OpeningUIManager : MonoBehaviour
 
     IEnumerator load(int num)
     {
-        transition.SetTrigger("Start");
-        yield return new WaitForSeconds(0.15f);
-        SceneManager.LoadScene(num);
+        start.SetTrigger("Start");
+        yield return new WaitForSeconds(duration);
+        SceneManager.LoadSceneAsync(num);
     }
 
     public void ExitGame(){
