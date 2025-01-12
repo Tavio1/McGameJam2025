683ff866b9209ca141b200513fbe41e402abc244
FlyingSheep22 <kevinbai222@gmail.com>
2025-01-12 10:58:04 -0500

    interior object fades

--- Assets/ExteriorFadeTwo.cs
+++ Assets/ExteriorFadeTwo.cs
@@ -3,26 +3,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ExteriorFade : MonoBehaviour
+public class ExteriorFadeTwo : MonoBehaviour
 {
 
-    [SerializeField] Material fadeMat;
+    [SerializeField] Material[] fadeMats;
     [SerializeField] float fadeSpeed = 5f;
-    private bool isInside = false;
+    // private bool isInside = false;
 
     
     void OnTriggerEnter(Collider other){
 
         if (other.tag != "Player") return;
         Debug.Log("entered");
-        if(isInside){
-            isInside = false;
-            FadeMat(true);
-        }
-        else{
-            isInside = true;
-            FadeMat(false);
-        }
+        FadeMat(false);
+    }
+
+    void OnTriggerExit(Collider other){
+        if (other.tag != "Player") return;
+        FadeMat(true);
     }
 
     // void OnTriggerExit(Collider other){
@@ -33,19 +31,28 @@ public class ExteriorFade : MonoBehaviour
 
 
     public void FadeMat(bool fadeIn){
-        StartCoroutine(FadeSequence(fadeIn));
+
+        foreach (Material mat in fadeMats){
+            StartCoroutine(FadeSequence(mat, fadeIn));
+        }
+
     }
 
-    IEnumerator FadeSequence(bool fadeIn){
+    IEnumerator FadeSequence(Material mat, bool fadeIn){
+        
         if (!fadeIn){
+            
+            mat.SetFloat("_Surface", 0); // 0 = Opaque
+            mat.DisableKeyword("_SURFACE_TYPE_TRANSPARENT");
+            mat.EnableKeyword("_SURFACE_TYPE_OPAQUE");
 
             float Alpha = 1f;
 
             while (Alpha > 0f){
                 Alpha -= Time.deltaTime;
-                Color col1 = fadeMat.color;
+                Color col1 = mat.color;
                 col1.a = Alpha;
-                fadeMat.color = col1;
+                mat.color = col1;
 
                 // Debug.Log("1 " + material.color.a);
 
@@ -54,13 +61,17 @@ public class ExteriorFade : MonoBehaviour
 
         } else{
 
+            mat.SetFloat("_Surface", 1); // 1 = Transparent
+            mat.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
+            mat.DisableKeyword("_SURFACE_TYPE_OPAQUE");
+
             float Alpha = 0f;
 
             while (Alpha < 1f){
                 Alpha += Time.deltaTime;
-                Color col1 = fadeMat.color;
+                Color col1 = mat.color;
                 col1.a = Alpha;
-                fadeMat.color = col1;
+                mat.color = col1;
 
                 // Debug.Log("1 " + material.color.a);
 
